06a047ec02eb998134f102afe1f6b10e2c590944
Valaeraa <valaeraa@gmail.com>
2020-08-11 16:25:49 +0100

    Added App4.Data project and configured EF Core

---
+++ src/App4/App4.Data/App4Context.cs
@@ -0,0 +1,23 @@
+﻿using App4.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App4.Data
+{
+    public class App4Context : DbContext
+    {
+        public App4Context()
+        {
+
+        }
+
+        public App4Context(DbContextOptions<App4Context> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
