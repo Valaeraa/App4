c7f745a0303592c4b1c4862bea40fe09ece8acff
Valaeraa <valaeraa@gmail.com>
2020-08-12 07:39:16 +0100

    Added UserModel and LeaderboardModel

---
+++ src/App4/App4.WebUI/Models/LeaderboardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App4.WebUI.Models
+{
+    public class LeaderboardModel
+    {
+        public int UserId { get; set; }
+        public int Score { get; set; }
+        public int GamesPlayed { get; set; }
+    }
+}
---
+++ src/App4/App4.WebUI/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App4.WebUI.Models
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+    }
+}
