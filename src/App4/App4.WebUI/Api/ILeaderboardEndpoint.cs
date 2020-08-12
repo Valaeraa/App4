7cae5da06afed5ef0c36fa7ca09f87bd3fe850c6
Valaeraa <valaeraa@gmail.com>
2020-08-12 07:42:52 +0100

    Added UsersEndpoint and LeaderboardEndpoint interfaces

---
+++ src/App4/App4.WebUI/Api/ILeaderboardEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using App4.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App4.WebUI.Api
+{
+    public interface ILeaderboardEndpoint
+    {
+        Task<IEnumerable<LeaderboardModel>> GetAll();
+        Task<LeaderboardModel> GetById(int id);
+        Task Update(int id, LeaderboardModel leaderboard);
+        Task Create(LeaderboardModel leaderboard);
+    }
+}
---
+++ src/App4/App4.WebUI/Api/IUsersEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using App4.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App4.WebUI.Api
+{
+    public interface IUsersEndpoint
+    {
+        Task<IEnumerable<UserModel>> GetAll();
+        Task<UserModel> GetById(int id);
+        Task Update(int id, UserModel user);
+        Task Create(UserModel user);
+    }
+}
