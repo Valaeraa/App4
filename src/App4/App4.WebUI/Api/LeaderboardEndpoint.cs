529cea4cabf30f06fad3321931805ee570c690e0
Valaeraa <valaeraa@gmail.com>
2020-08-12 07:52:59 +0100

    Added the LEaderboardEndpoint class

---
+++ src/App4/App4.WebUI/Api/LeaderboardEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using App4.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App4.WebUI.Api
+{
+    public class LeaderboardEndpoint : ILeaderboardEndpoint
+    {
+        private readonly IAPIHelper _apiHelper;
+
+        public LeaderboardEndpoint(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public Task Create(LeaderboardModel leaderboard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LeaderboardModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaderboardModel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(int id, LeaderboardModel leaderboard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
