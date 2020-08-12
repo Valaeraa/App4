c9c40acd23b2b8b06d00bc69eb8beaafbce8359f
Valaeraa <valaeraa@gmail.com>
2020-08-12 07:07:55 +0100

    Added MVC UI project

---
+++ src/App4/App4.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using App4.WebUI.Models;
+using App4.WebUI.Api;
+
+namespace App4.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IAPIHelper _apiHelper;
+
+        public HomeController(ILogger<HomeController> logger, IAPIHelper apiHelper)
+        {
+            _logger = logger;
+            _apiHelper = apiHelper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
