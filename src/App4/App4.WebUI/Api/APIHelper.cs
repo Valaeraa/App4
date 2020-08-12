30f5a612d93602dc563acf92dfd327910130590f
Valaeraa <valaeraa@gmail.com>
2020-08-12 07:40:44 +0100

    Added APIHelper class

---
+++ src/App4/App4.WebUI/Api/APIHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace App4.WebUI.Api
+{
+    public class APIHelper : IAPIHelper
+    {
+        public APIHelper(IConfiguration config)
+        {
+            _config = config;
+
+            InitializeClient();
+        }
+
+        private HttpClient _apiClient;
+        private readonly IConfiguration _config;
+
+        public HttpClient ApiClient
+        {
+            get
+            {
+                return _apiClient;
+            }
+        }
+
+        private void InitializeClient()
+        {
+            var api = _config.GetValue<string>("ApiKey");
+
+            _apiClient = new HttpClient();
+
+            _apiClient.BaseAddress = new Uri(api);
+            _apiClient.DefaultRequestHeaders.Accept.Clear();
+            _apiClient.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ src/App4/App4.WebUI/Api/IAPIHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace App4.WebUI.Api
+{
+    public interface IAPIHelper
+    {
+        HttpClient ApiClient { get; }
+    }
+}
