042d9fb30cf96c20b5ff6406e0e6e7e91075b5dc
Valaeraa <valaeraa@gmail.com>
2020-08-12 10:01:57 +0100

    Implemented the logic for the LeaderboardEndpoint

--- src/App4/App4.WebUI/Api/LeaderboardEndpoint.cs
+++ src/App4/App4.WebUI/Api/LeaderboardEndpoint.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace App4.WebUI.Api
 {
     public class LeaderboardEndpoint : ILeaderboardEndpoint
     {
+        private const string _requestUri = "api/Leaderboard";
         private readonly IAPIHelper _apiHelper;
 
         public LeaderboardEndpoint(IAPIHelper apiHelper)
@@ -15,24 +17,68 @@ namespace App4.WebUI.Api
             _apiHelper = apiHelper;
         }
 
-        public Task Create(LeaderboardModel leaderboard)
+        public async Task Create(LeaderboardModel leaderboard)
         {
-            throw new NotImplementedException();
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync(_requestUri, leaderboard))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    response.StatusCode = System.Net.HttpStatusCode.Created;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
         }
 
-        public Task<IEnumerable<LeaderboardModel>> GetAll()
+        public async Task<IEnumerable<LeaderboardModel>> GetAll()
         {
-            throw new NotImplementedException();
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync(_requestUri))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<LeaderboardModel>>();
+
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
         }
 
-        public Task<LeaderboardModel> GetById(int id)
+        public async Task<LeaderboardModel> GetById(int id)
         {
-            throw new NotImplementedException();
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync($"{_requestUri}/{id}"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<LeaderboardModel>();
+
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
         }
 
-        public Task Update(int id, LeaderboardModel leaderboard)
+        public async Task Update(int id, LeaderboardModel leaderboard)
         {
-            throw new NotImplementedException();
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.PutAsJsonAsync($"{_requestUri}/{id}", leaderboard))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    response.StatusCode = System.Net.HttpStatusCode.OK;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
         }
     }
 }
