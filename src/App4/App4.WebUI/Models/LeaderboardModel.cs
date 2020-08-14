26840280755ac4eb9e336ac4a6dc34441fe20cbe
Valaeraa <valaeraa@gmail.com>
2020-08-14 15:06:07 +0100

    Added validations to the UserController and LeaderboardController

--- src/App4/App4.WebUI/Models/LeaderboardModel.cs
+++ src/App4/App4.WebUI/Models/LeaderboardModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,16 @@ namespace App4.WebUI.Models
     public class LeaderboardModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please select a user")]
+        [Display(Name = "User Id")]
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please add a score")]
         public int Score { get; set; }
+
+        [Required(ErrorMessage = "Please add the number of games played")]
+        [Display(Name = "Games Played")]
         public int GamesPlayed { get; set; }
     }
 }
